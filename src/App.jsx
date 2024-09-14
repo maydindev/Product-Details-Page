9eea83316a9741fc5fd3a167258a9b3bd38579cc
maydindev <maydin.dev@outlook.com>
2024-09-14 12:23:02 +0300

    "Updated dependencies, added routing to App component, and modified Product and ProductDetails components to use react-router-dom"

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
-import StoreFront from './pages/StoreFront'
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import StoreFront from "./pages/StoreFront";
+import ProductDetails from "./pages/ProductDetails";
+
 export default function App() {
-  return <StoreFront />
+  return (  
+    <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<StoreFront />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
 }
+
+/*
+<Route path="/" element={<StoreFront />}>
+
+<Route path="/products/:id" element={<ProductDetails/>}/>
+*/
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,26 +1,32 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Product(props) {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-  const { details } = props
+  const { details } = props;
 
   function handleIncrementClick() {
-    setCount((c) => c + 1)
+    setCount((c) => c + 1);
   }
   function handleDecrementClick() {
     if (count > 0) {
-      setCount((c) => c - 1)
+      setCount((c) => c - 1);
     }
   }
-
+ 
+  //Link ile ürüne tıklayınca /products/id sayfasına yönlendirdik.
   return (
-    <div className='product'>
-      <img src={details.image} width='50' alt={details.name} />
-      <div className='product-info'>
-        <h2>{details.name}</h2>
-        <p>{details.description}</p>
+    
+      <div className="product">
+        <Link to={`/products/${details.id}`}>
+        <img src={details.image} width="50" alt={details.name} />
+        <div className="product-info">
+          <h2>{details.name}</h2>
+          <p>{details.description}</p>
+        </div>
+        </Link>
       </div>
-    </div>
-  )
+    
+  );
 }
